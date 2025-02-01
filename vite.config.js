55508810365bb4a8d14a94cb6e75967a8c59a3f5
HarryTsai <sleepingturtle990866@gmail.com>
2025-02-01 14:33:31 +0800

    finish work

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   //前者改成github的庫
-  base: process.env.NODE_ENV === 'production' ? '/react-work-2/':'/',
+  base: process.env.NODE_ENV === 'production' ? '/react-work-3/':'/',
   plugins: [react()],
 })
